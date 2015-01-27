60fe6cfd59fe29fc21b0d31b2365869b157c39ad
Paarth <paarthk@gmail.com>
2015-01-26 20:04:46 -0500

    began fleshing out jwt

--- jwt.ml
+++ jwt.ml
@@ -1,10 +1,45 @@
 (* A simple JWT library for OCaml *)
 
-module JWT = struct
-	type algorithm = 
-		| HS256
+type algorithm = 
+	| None
+	| HS256
+	| HS384
+	| HS512
 
-	let encode payload algorithm key = ()
+type jwt_header = {
+	alg: algorithm
+}
+type jwt_payload = {
+	claims: (string * string) list
+}
 
-	let decode payload algorithm key = ()
+type t = {
+	header: jwt_header;
+	payload: jwt_payload;
+	signature: string option;
+}
+
+module SignedToken = struct
+	open Nocrypto
+	open Nocrypto.Uncommon
+	open Nocrypto.Hash
+
+	let signing_func = function
+		| None -> (fun ~key _ -> Cstruct.of_string "")
+		| HS256 -> SHA256.hmac
+		| HS384 -> SHA384.hmac
+		| HS512 -> SHA512.hmac
+
+	let signature data alg key = 
+		(signing_func alg) ~key:key data
+
+	let verify data input_sig alg key = match input_sig with
+		| Some s -> s = (signature data alg key)
+		| None -> false
 end
+
+let encode payload algorithm key = ()
+
+let verify key token =
+	SignedToken.verify
+let decode token key = ()
