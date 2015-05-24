30fe49734137e3adcec0292bbdd0192d49669c92
Paarth <paarthk@gmail.com>
2015-05-24 19:43:08 -0400

    Began the encoding logic

--- jwt.ml
+++ jwt.ml
@@ -7,18 +7,16 @@ type algorithm =
 	| HS384
 	| HS512
 
-type jwt_header = {
-	alg: algorithm option
-}
-type jwt_payload = {
-	claims: (string * string) list
-}
+type jwt_header =
+	{ alg: algorithm option }
 
-type t = {
-	header: jwt_header;
+type jwt_payload = 
+	{ claims: (string * string) list }
+
+type t =
+  {	header: jwt_header;
 	payload: jwt_payload;
-	signature: string option;
-}
+	signature: string option; }
 
 module SignedToken = struct
 	open Nocrypto
@@ -68,6 +66,11 @@ let alg_of_str = function
 	| "None" -> None
 	| _ -> raise (Jwt_format_error "Unsupported algorithm")
 
+let str_of_alg = function
+	| Some HS256 -> "HS256"
+	| Some HS384 -> "HS384"
+	| Some HS512 -> "HS512"
+	| None -> "None"
 
 let dequote str =
 	let len = String.length str in
@@ -75,6 +78,8 @@ let dequote str =
 		String.sub str 1 (len - 2)
 	else
 		str
+
+let enquote str = String.concat "" ["\"";str;"\""]
 
 let parse token =
 	token
@@ -105,7 +110,27 @@ let parse token =
 			}
 		| _ -> raise (Jwt_format_error "Improper input. Expected a period-delimited string with two or three parts")
 
-let encode payload algorithm key = ()
+let json_of_header h =
+	let alg_str = (str_of_alg(h.alg)) in
+	`Assoc
+	[
+		("type", `String "JWT");
+		("alg", `String alg_str)
+
+	]
+
+let json_of_claims claims = 
+	let rec convert_claims = function
+		| ((t,c)::tl) -> (t,`String  c)::convert_claims(tl)
+		| [] -> []
+	in
+	`Assoc (convert_claims claims)
+
+let encode token =
+	let headj = json_of_header token.header in
+	let claimsj = json_of_claims token.payload.claims in
+	let compile x = B64.encode (Yojson.Basic.to_string x) in
+	String.concat "." [compile headj ; compile claimsj ; "signature"]
 
 let verify key token =
 	SignedToken.verify
