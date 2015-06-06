f678fdf0de191b6ee66f760db774e38738b37a19
Paarth <paarthk@gmail.com>
2015-06-06 00:58:15 -0400

    Added registered claims to mli

--- jwt.mli
+++ jwt.mli
@@ -14,6 +14,24 @@ val encode : key -> t -> string
 
 val decode : key -> string -> t option
 
+(* claim manipulation *)
 val claim : string -> t -> Yojson.Basic.json
 
 val add_claim : string -> Yojson.Basic.json -> t -> t
+
+(* Meta info *)
+val alg : t -> algorithm option
+(* I actually kind of hate this because 
+NO alg specified, invalid alg specified,
+and unsupported alg specified are three
+different things *)
+
+(* registered claims *)
+val iss : t -> string option
+val sub : t -> string option
+val aud : t -> string option
+
+val exp : t -> int option
+val nbf : t -> int option
+val iat : t -> int option
+val jti : t -> int option
